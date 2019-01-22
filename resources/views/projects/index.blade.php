c0a493f0b860e1f1d5e90890ba322e135bed0813
Douglas Mburu <douglasmburu105@gmail.com>
2019-01-22 17:35:11 +0300

    grid spacing

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
 
-    <div class="flex">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-grey text-sm font-normal">New Project</h2>
+            <a href="/projects/create" class="no-underline bg-blue text-white rounded-lg text-sm py-2 px-5" style="box-shadow: 0 2px 7px 0 #b0eaff; ">New Project</a>
+        </div>
+
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px">
-                <h3 class="font-normal text-xl py-4"> {{$project->title}} </h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow " style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 border-l-4 mb-3 border-blue-light -ml-5 pl-4">
+                        <a href="{{$project->path()}}" class="text-black no-underline">{{$project->title}}</a> </h3>
 
-                <div class="text-grey">
-                    {{str_limit($project->description, 100)}}
+                    <div class="text-grey">
+                        {{str_limit($project->description, 100)}}
+                    </div>
                 </div>
             </div>
         @empty
             <div>No projects here</div>
         @endforelse
-    </div>
+    </main>
 @endsection
