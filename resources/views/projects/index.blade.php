c1c270da31a351d3d94394f9a355c9e220999dc8
Douglas Mburu <douglasmburu105@gmail.com>
2019-01-22 16:24:20 +0300

    use tailwind css

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,16 +4,18 @@
     <div class="flex items-center mb-3">
         <a href="/projects/create">New Project</a>
     </div>
-     <ul>
-         @forelse($projects as $project)
-             <li>
-                 <a href="{{$project->path()}}">
-                     {{$project->title}}
-                 </a>
-             </li>
-             <li>{{$project->description}}</li>
-         @empty
-             <li>No projects here</li>
-         @endforelse
-     </ul>
+
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white p-5 mr-4 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="font-normal text-xl py-4"> {{$project->title}} </h3>
+
+                <div class="text-grey">
+                    {{str_limit($project->description, 100)}}
+                </div>
+            </div>
+        @empty
+            <div>No projects here</div>
+        @endforelse
+    </div>
 @endsection
