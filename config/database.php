45dddbc1d6b88f531ced86be1f97423ffa37ce70
Douglas Mburu <douglasmburu105@gmail.com>
2019-01-23 09:32:21 +0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL = parse_url('postgres://ezymqibntnqxrj:75493068010c26558edb03abf48103b0c0aab063e2beb631a1b815566c310d2c@ec2-50-17-193-83.compute-1.amazonaws.com:5432/dbpufkcvpqcdv1');
 return [
 
     /*
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+//    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +59,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
