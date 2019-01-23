15e8866071d3853bc366e7f0594dbcf5d725e994
Douglas Mburu <douglasmburu105@gmail.com>
2019-01-23 09:25:39 +0300

    first laravel deployment to heroku

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <header class="flex items-center mb-3 py-4">
 
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-grey text-sm font-normal">New Project</h2>
             <a href="/projects/create" class="no-underline bg-blue text-white rounded-lg text-sm py-2 px-5" style="box-shadow: 0 2px 7px 0 #b0eaff; ">New Project</a>
         </div>
